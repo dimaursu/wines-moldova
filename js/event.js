cf1116f1896fa2d342026627c016adac4f0e53d7
VLADIMIR MELNIC <win32hack@gmail.com>
2014-12-21 11:15:35 +0200

    Added event item view page.

---
+++ js/event.js
@@ -0,0 +1,47 @@
+/**
+ * Wine food matching model/controller.
+ */
+$(document).ready(function () {
+
+  var tt = new TopcoatTouch();
+
+  tt.on('click', 'li.eventItem', function () {
+
+    tt.goTo('eventItemTemplate');
+
+    var wineMatchesList = $('#wineMatchesList');
+
+    // Define fetch URL.
+    var url = dataSource + "events/" + $(this).data('id') + "?filter[language]=en";
+
+    // Get json from URL.
+    $.getJSON(url, function (data) {
+
+      var element = data.data[0];
+
+      var itemData = {
+        thumbnail: element.image.styles.large,
+        id: element.id,
+        label: element.label,
+        description: element.body,
+        location: element.location,
+        date: element.date,
+        price: element.price,
+        buy: element.buy
+      };
+
+      // Process and render event item.
+      var source = $("#event-item-template").html();
+      var compiled = dust.compile(source, "wine");
+      dust.loadSource(compiled);
+      dust.render("wine", itemData, function (err, out) {
+        $("#eventItemContent").html(out);
+      });
+
+    });
+
+
+
+  });
+
+});
