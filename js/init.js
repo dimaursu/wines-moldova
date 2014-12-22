66cf0cc96fc4b34d63dfa4b09aaf40c3c3702132
VLADIMIR MELNIC <win32hack@gmail.com>
2014-12-22 03:37:20 +0200

    Update.

--- js/init.js
+++ js/init.js
@@ -1,3 +1,11 @@
 /**
  * Init app json calls.
  */
+
+$.ajaxSetup({
+  async: false
+});
+
+$.getJSON(dataSource + "wines?sort=label&filter[language]=en", function (data) {
+  window.winesList = data;
+});
